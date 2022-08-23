7520b2cc4257119bf9c78e025e36f8e929c2c398
KateNepomnyacshikh <verge666@yandex.ru>
2022-08-23 08:57:36 +0300

    Add files HomeWorke №1

---
+++ Three/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("число четное");
+}
+else
+{
+    Console.WriteLine("число нечетное");
+}
