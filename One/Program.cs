e5a5a1f4e50b8c164362a78575d4969597478096
KateNepomnyacshikh <verge666@yandex.ru>
2022-08-23 08:57:00 +0300

    Add files HomeWorke №1

---
+++ One/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число А: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB) 
+{
+    Console.WriteLine( numberA + " больше " + numberB);
+}
+else if (numberA < numberB)
+{
+    Console.WriteLine(numberB + " больше " + numberA);
+}
+else
+{
+ Console.WriteLine(numberB + " равно " + numberA);   
+}
