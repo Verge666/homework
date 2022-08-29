0fb324dbd55b3fe2bb3399c30d4b704dd107a3b8
KateNepomnyacshikh <verge666@yandex.ru>
2022-08-29 04:31:44 +0300

    add six

---
+++ Six/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("введите число");
+int n = int.Parse(Console.ReadLine());
+int result;
+int i = 1000;
+int j = 1;
+if (n < 100)
+{
+ Console.WriteLine("у числа нет третьей цифры");   
+}
+else
+{
+    while (n > i)
+        {
+            i*=10;
+            j*=10;
+        }
+    result = (n / j) % 10;
+    Console.WriteLine("третья цифра числа " + n + " - " + result);    
+}
