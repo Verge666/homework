756dc5331c41b47de43bda6f7ad6e1fbd8eee582
KateNepomnyacshikh <verge666@yandex.ru>
2022-08-29 04:30:46 +0300

    add five

---
+++ Five/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите трехзначное число");
+int n = int.Parse(Console.ReadLine());
+int result;
+if ((n > 999) || (n < 100))
+{
+    Console.WriteLine("число не трехзначное, введите другое число");
+}
+else
+{
+    result = (n / 10) % 10;
+    Console.WriteLine("вторая цифра числа " + n + " - " + result);
+}
