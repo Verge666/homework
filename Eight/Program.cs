a02db6e786bc5516279791fa5b4a6044c175af18
KateNepomnyacshikh <verge666@yandex.ru>
2022-09-01 09:45:31 +0300

    add Eight

---
+++ Eight/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("введите пятизначное число");
+string n = Console.ReadLine();
+if ((int.Parse(n) > 99999 ) || (int.Parse(n) < 10000))
+    {
+        Console.WriteLine("введенное число не пятизначное");
+    }
+else
+    {
+    if ((n[0] == n [4]) && (n[1] == n[3]))
+        {
+            Console.WriteLine("число " + n + " является палиндромом");
+        }
+    else
+        {
+            Console.WriteLine("число " + n + " не является палиндромом");
+        }
+    }
