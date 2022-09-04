8f3b59aec60fae2cc9d854ff7385ea4842b5aa9e
KateNepomnyacshikh <verge666@yandex.ru>
2022-09-04 08:03:59 +0300

    add Eleven

---
+++ Eleven/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("введите число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число B: ");
+int b = int.Parse(Console.ReadLine());
+double result = Math.Pow(a, b);
+Console.WriteLine("результат: " + result);
