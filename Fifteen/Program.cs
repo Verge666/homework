ac7e2ba707230d171fdaa85dd0d85360b71d4fbf
KateNepomnyacshikh <verge666@yandex.ru>
2022-09-08 17:47:16 +0300

    add Fifteen

---
+++ Fifteen/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите размер массива: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+int sum = 0;
+Random rnd = new Random();
+for (int i = 0; i < n; i++)
+    {
+        array[i] = rnd.Next();
+        Console.WriteLine(array[i]);
+        if (i % 2 == 0)
+        {
+            sum += array[i];
+        }
+    }
+Console.WriteLine("сумма нечетных элементов массива: " + sum);
