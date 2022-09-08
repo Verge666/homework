5db44f921e66afb4443d67759d09e978c158179e
KateNepomnyacshikh <verge666@yandex.ru>
2022-09-08 17:46:24 +0300

    add Fourteen

---
+++ Fourteen/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("введите длину массива:  ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int [n];
+int counter = 0;
+Random rnd = new Random();
+for (int i = 0; i < n; i++)
+{
+    array[i] = rnd.Next(100, 999);
+    Console.WriteLine(array[i]);
+    if (array[i] % 2 == 0)
+    {
+        counter += 1;
+    }
+}
+Console.WriteLine("четных чисел в массиве: " + counter);
