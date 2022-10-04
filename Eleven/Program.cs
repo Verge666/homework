b19b6f33907612cc964093958dbba63ce8fa4a29
KateNepomnyacshikh <verge666@yandex.ru>
2022-10-04 20:03:41 +0300

    add Eleven

---
+++ Eleven/Program.cs
@@ -0,0 +1,22 @@
+﻿int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+int Func_ak(int m, int n)
+{
+if (m == 0)
+    {
+        return n + 1;
+    }
+else if (m > 0 && n ==0)
+    {
+        return Func_ak (m -1, 1);
+    }    
+else if (m > 0 && n > 0)
+    {
+        return Func_ak (m - 1, Func_ak(m, n-1));
+    }  
+else 
+    {
+        return 0;
+    }          
+}
+Console.WriteLine(Func_ak(m, n));
